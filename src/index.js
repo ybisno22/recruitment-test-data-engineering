387d370ead922ee5aa20ed682410bf253d219dd4
Yair Bisnowaty <yair@digitalkinetics.net>
2024-09-16 11:32:46 +0200

    changed routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import ip from 'ip';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import Response from './util/response.js';
+import HttpStatus from './util/httpStatus.js';
 
 // Load env variables
 dotenv.config();
@@ -15,9 +16,15 @@ const PORT = process.env.SERVER_PORT || 3000;
 app.use(cors({ origin: '*'}));
 app.use(express.json());
 
-// Root route for basic testing
+// Root route for testing
 app.get('/', (req, res) => {
-  res.send('Node application is running!');
+    res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'People and Places API v1.0.0'));
+});
+
+// Fallback route for undefined routes
+app.all('*', (req, res) => {
+    res.status(HttpStatus.NOT_FOUND.code)
+      .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Server route does not exist'));
 });
 
 // Start the server
