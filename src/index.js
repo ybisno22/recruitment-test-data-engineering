3663cb5265a44661218e18eec36aa0f1203f01c1
Yair Bisnowaty <yair@digitalkinetics.net>
2024-09-16 11:25:46 +0200

    index.js for node app

--- src/index.js
+++ src/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import ip from 'ip';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+
+// Load env variables
+dotenv.config();
+
+// Initialise app
+const app = express();
+const PORT = process.env.SERVER_PORT || 3000;
+
+// Middleware
+app.use(cors({ origin: '*'}));
+app.use(express.json());
+
+// Root route for basic testing
+app.get('/', (req, res) => {
+  res.send('Node application is running!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${ip.address()}:${PORT}`);
+});
